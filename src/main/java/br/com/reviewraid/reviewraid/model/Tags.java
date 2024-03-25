0341f5dc5121719007dc36af20d7534ed4e696e9
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-03-25 11:17:31 -0300

    Added Bean Validation

--- src/main/java/br/com/reviewraid/reviewraid/model/Tags.java
+++ src/main/java/br/com/reviewraid/reviewraid/model/Tags.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,6 +14,11 @@ public class Tags {
     
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Nome da tag é obrigatória")
+    @Size(min = 3, max = 255, message = "O nome da tag deve ter pelo menos 3 caracteres")
     private String nome;
+
+    @Size(min = 3, max = 255, message = "A descrição deve ter pelo menos 3 caracteres")
     private String descricao;
 }
