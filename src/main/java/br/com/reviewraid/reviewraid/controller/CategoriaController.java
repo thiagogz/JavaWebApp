b75fdff8f00400d6194b6f77c8e36b31ed34730c
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-04-28 22:54:54 -0300

    Updated api

--- src/main/java/br/com/reviewraid/reviewraid/controller/CategoriaController.java
+++ src/main/java/br/com/reviewraid/reviewraid/controller/CategoriaController.java
@@ -3,20 +3,25 @@ package br.com.reviewraid.reviewraid.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.reviewraid.reviewraid.model.Categoria;
 import br.com.reviewraid.reviewraid.repository.CategoriaRepository;
 import jakarta.validation.Valid;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -48,4 +53,26 @@ public class CategoriaController {
                 .orElse(ResponseEntity.notFound().build());
     }
     
+    @PutMapping("{id}")
+    public Categoria atualizarCategoria(@PathVariable Long id, @RequestBody Categoria categoria){
+        
+        verificarExistenciaCategoria(id);
+        categoria.setId(id);
+        return repository.save(categoria);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void deletarJogo(@PathVariable Long id){
+        verificarExistenciaCategoria(id);
+        repository.deleteById(id);
+    }
+
+    private void verificarExistenciaCategoria(Long id) {
+        repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Não existe categoria com o id informado. Consulte lista em /categorias."));
+    }
 }
--- src/main/java/br/com/reviewraid/reviewraid/model/Categoria.java
+++ src/main/java/br/com/reviewraid/reviewraid/model/Categoria.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria extends Object{
     
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
